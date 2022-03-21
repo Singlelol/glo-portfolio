343d1f2809893dfa6d882feeb641838551b7e5f3
Singlelol <95968986+Singlelol@users.noreply.github.com>
2022-03-21 22:39:05 +0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,10 +1,12 @@
-const modalButton = document.querySelector('.more')
+const modalButtons = document.querySelectorAll('.more')
 const modal = document.querySelector('.modal')
 const overlay = modal.querySelector('.overlay')
 const closeButton = modal.querySelector('.modal__close')
 
-modalButton.addEventListener('click', () => {
-    modal.classList.remove('hidden')
+modalButtons.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        modal.classList.remove('hidden')
+    })
 })
 
 overlay.addEventListener('click', () => {
