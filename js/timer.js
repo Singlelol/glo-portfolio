618af44af9f7986188c1283e8e3681432f466fce
Singlelol <95968986+Singlelol@users.noreply.github.com>
2022-03-22 19:42:54 +0300

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -13,24 +13,28 @@ const updateClock = () => {
     const seconds = Math.floor(timeRemaining % 60)
     const days = Math.floor(timeRemaining / 60 / 60 / 24)
 
+    let fDays
+
+    if (days === 0) {
+        fDays = ''
+    } else if (days === 1) {
+        fDays = days + ' день '
+    } else if (days <= 4) {
+        fDays = days + ' дня '
+    } else {
+        fDays = days + ' дней '
+    }
+
     const fHours = hours < 10 ? '0' + hours : hours
     const fMinutes = minutes < 10 ? '0' + minutes : minutes
     const fSeconds = seconds < 10 ? '0' + seconds : seconds
-    const fDays = days < 4 ? days + ' дня ' : days + ' дней '
+    //const fDays = days < 4 ? days + ' дня ' : days + ' дней '
 
     timerBlock.textContent = `${fDays}${fHours}:${fMinutes}:${fSeconds}`
 
     if (timeRemaining <= 0 ) {
         clearInterval(interval)
         timerBlock.textContent = `00:00:00`
-    }
-    
-    if (days = 1 ) {
-        const fDays = days + ' день '
-    }
-    
-    if (days <= 0 ) {
-        const fDays = ''
     }
 }
 
