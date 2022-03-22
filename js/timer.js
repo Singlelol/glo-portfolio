235d36071e4061c3030f5959f24845dbf5ed4436
Singlelol <95968986+Singlelol@users.noreply.github.com>
2022-03-22 12:13:42 +0300

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -8,15 +8,16 @@ const updateClock = () => {
     const dateDeadline = new Date(deadline).getTime()
     const timeRemaining = (dateDeadline - date) / 1000
 
-    const hours = Math.floor(timeRemaining / 60 / 60)
+    const hours = Math.floor((timeRemaining / 60 / 60) % 24)
     const minutes = Math.floor((timeRemaining / 60) % 60)
     const seconds = Math.floor(timeRemaining % 60)
+    const days = Math.floor(timeRemaining / 60 / 60 / 24)
 
     const fHours = hours < 10 ? '0' + hours : hours
     const fMinutes = minutes < 10 ? '0' + minutes : minutes
     const fSeconds = seconds < 10 ? '0' + seconds : seconds
 
-    timerBlock.textContent = `${fHours}:${fMinutes}:${fSeconds}`
+    timerBlock.textContent = `${days}:${fHours}:${fMinutes}:${fSeconds}`
 
     if (timeRemaining <= 0 ) {
         clearInterval(interval)
